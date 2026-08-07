22a71ac8dc55f7d446df1b92580f188de69f73b9
Onurcan Özovalı <41977097+onurcanozovali@users.noreply.github.com>
2026-08-07 16:11:52 +0300

    Fix formatting of googleServicesFile paths in config

--- app.config.ts
+++ app.config.ts
@@ -4,14 +4,18 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: config.name ?? 'Devrem',
   slug: config.slug ?? 'devrem',
+
   android: {
     ...config.android,
     googleServicesFile:
-      process.env.FIREBASE_ANDROID_CONFIG_FILE ?? './config/firebase/development/google-services.json',
+      process.env.FIREBASE_ANDROID_CONFIG_FILE ??
+      './config/firebase/development/google-services.json',
   },
+
   ios: {
     ...config.ios,
     googleServicesFile:
-      process.env.FIREBASE_IOS_CONFIG_FILE ?? './config/firebase/development/GoogleService-Info.plist',
+      process.env.FIREBASE_IOS_CONFIG_FILE ??
+      './config/firebase/development/GoogleService-Info.plist',
   },
 });
